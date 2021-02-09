9b7a45a709ae790acea0ff3754ebe6955504b4e5
David Silva <david.arsilva@gmail.com>
2021-02-09 16:12:15 +0000

    setup bulma

--- src/main/kotlin/pt/ulusofona/tfc/trabalho/TrabalhoApplication.kt
+++ src/main/kotlin/pt/ulusofona/tfc/trabalho/TrabalhoApplication.kt
@@ -7,6 +7,6 @@ import org.springframework.boot.runApplication
 class TrabalhoApplication
 
 fun main(args: Array<String>) {
-    println("Teste commit" + "nice")
+    println("Teste commit")
     runApplication<TrabalhoApplication>(*args)
 }
