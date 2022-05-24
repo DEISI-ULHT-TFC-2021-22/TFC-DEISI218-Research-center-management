1cd2962c06a80e86ed38e924bfbbc8cb4d11b63c
Marco-21905157 <marcoviana03@gmail.com>
2022-05-24 20:07:16 +0100

    researcher corrigido

--- src/main/kotlin/pt/ulusofona/tfc/trabalho/dao/scientificActivities/ProjectResearcher.kt
+++ src/main/kotlin/pt/ulusofona/tfc/trabalho/dao/scientificActivities/ProjectResearcher.kt
@@ -1,11 +1,18 @@
 package pt.ulusofona.tfc.trabalho.dao.scientificActivities
 
+import java.io.Serializable
 import javax.persistence.Entity
 import javax.persistence.Id
+import javax.persistence.IdClass
 
 @Entity
+@IdClass(ProjectResearcherId::class)
 data class ProjectResearcher(
         @Id
-        val projectId: Long,
-        val researcherId: String
+        val researcherId: String,
+
+        @Id
+        val projectId: Long
 )
+
+data class ProjectResearcherId(val researcherId: String, val projectId: String) : Serializable
